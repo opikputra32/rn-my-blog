02771f86dd912d6275c09e77dd77c139812a44b0
opikputra32 <opikputra32@gmail.com>
2022-02-18 01:27:30 +0700

    reconstructuring form dry code to components

---
+++ src/components/BlogPostForm.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+
+const BlogPostForm = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Enter Title:</Text>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+      />
+      <Text style={styles.label}>Enter Content:</Text>
+      <TextInput
+        style={styles.input}
+        value={content}
+        onChangeText={(text) => setContent(text)}
+      />
+      <Button
+        title="Save Blog Post"
+        onPress={() => {
+          // // When adding the data then navigation to home
+          // addBlogPost(title, content, () => {
+          //   navigation.navigate("Index");
+          // });
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 10,
+    flex: 1,
+  },
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 10,
+  },
+  label: {
+    fontSize: 20,
+  },
+  buttonStyle: {
+    marginVertical: 10,
+  },
+});
+
+export default BlogPostForm;
--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,56 +1,14 @@
-import React, { useContext, useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
-import { Context as BlogContext } from '../context/BlogContext';
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
+import { Context as BlogContext } from "../context/BlogContext";
+import BlogPostForm from "../components/BlogPostForm";
 
 const CreateScreen = ({ navigation }) => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
   const { addBlogPost } = useContext(BlogContext);
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Enter Title:</Text>
-      <TextInput
-        style={styles.input}
-        value={title}
-        onChangeText={(text) => setTitle(text)}
-      />
-      <Text style={styles.label}>Enter Content:</Text>
-      <TextInput
-        style={styles.input}
-        value={content}
-        onChangeText={(text) => setContent(text)}
-      />
-      <Button
-        title="Add Blog Post"
-        onPress={() => {
-          // When adding the data then navigation to home
-          addBlogPost(title, content, () => {
-            navigation.navigate('Index');
-          });
-        }}
-      />
-    </View>
-  );
+  return <BlogPostForm />;
 };
 
-const styles = StyleSheet.create({
-  container: {
-    marginHorizontal: 10,
-    flex: 1,
-  },
-  input: {
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: 'black',
-    marginBottom: 10,
-  },
-  label: {
-    fontSize: 20,
-  },
-  buttonStyle: {
-    marginVertical: 10,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default CreateScreen;
