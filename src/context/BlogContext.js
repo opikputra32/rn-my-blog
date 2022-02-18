2ca99fc621717026b8c9bf0b6a3e7679eea1eb18
opikputra32 <opikputra32@gmail.com>
2022-02-18 15:46:26 +0700

    Cleanup context to axios api

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -12,15 +12,6 @@ const blogReducer = (state, action) => {
       });
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
-    case "add_blogpost":
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 9999),
-          title: action.payload.title,
-          content: action.payload.content,
-        },
-      ];
     default:
       return state;
   }
@@ -37,8 +28,6 @@ const getBlogPosts = (dispatch) => {
 const addBlogPost = (dispatch) => {
   return async (title, content, callback) => {
     await jsonServer.post("/blogposts", { title, content });
-
-    // dispatch({ type: "add_blogpost", payload: { title, content } });
     if (callback) {
       callback();
     }
@@ -46,13 +35,15 @@ const addBlogPost = (dispatch) => {
 };
 
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
+  return async (id) => {
+    await jsonServer.delete(`/blogposts/${id}`);
     dispatch({ type: "delete_blogpost", payload: id });
   };
 };
 
 const editBlogPost = (dispatch) => {
-  return (id, title, content, callback) => {
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`/blogposts/${id}`, { title, content });
     dispatch({
       type: "edit_blogpost",
       payload: { id, title, content },
